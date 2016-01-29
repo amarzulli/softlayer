1cc95c75abd75bc2d0e66732343e1759d3a15eae
amarzulli <andy.j@outlook.com>
2016-01-29 18:19:00 +1100

    Update domain join.bat

--- domain join.bat
+++ domain join.bat
@@ -1,3 +1,3 @@
-netsh interface ipv4 add dnsserver "PrivateNetwork-A" address=10.118.136.23 index=1
+netsh interface ipv4 set dnsservers "PrivateNetwork-A" static 10.118.136.23 primary
 netdom join /d:contoso.com Server1 /ud:contoso\administrator /pd:YF5bwplN /reboot
 
