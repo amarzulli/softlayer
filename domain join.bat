1eb0ea79675036517f61bfd2633d3ed4c404717a
amarzulli <andy.j@outlook.com>
2016-01-29 18:23:55 +1100

    Update domain join.bat

--- domain join.bat
+++ domain join.bat
@@ -1,3 +1,3 @@
 netsh interface ipv4 set dnsservers "PrivateNetwork-A" static 10.118.136.23 primary
-netdom join /d:contoso.com Server1 /ud:contoso\administrator /pd:YF5bwplN /reboot
+netdom join /d:contoso Server1 /ud:contoso\administrator /pd:YF5bwplN /reboot
 
