b44d222c9a3db1821d8161b3041ab5599d573fd5
amarzulli <andy.j@outlook.com>
2018-03-07 17:43:32 +1100

    Create join_domain.bat

---
+++ join_domain.bat
@@ -0,0 +1,1 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/amarzulli/softlayer/master/join_domain.ps1'))"
