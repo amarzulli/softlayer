fc38d5bb9bb49a5831de2bbb6f3586bf51627b67
amarzulli <andy.j@outlook.com>
2018-03-08 12:06:15 +1100

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,5 +1,5 @@
 Set-DNSClientServerAddress -InterfaceIndex 2 -ServerAddresses '10.118.86.66'
-Install-WindowsFeature -Name 'Hyper-V' -IncludeAllSubFeature -IncludeMangementTools
+Install-WindowsFeature -Name 'Hyper-V' -IncludeAllSubFeature -IncludeManagementTools
 $Password = 'BKzHGy5c'
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("administrator", $secpasswd)
