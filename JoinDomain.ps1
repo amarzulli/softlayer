531b5d384ce28af97b74bfd1385f56d512a4f014
amarzulli <andy.j@outlook.com>
2018-03-08 11:17:04 +1100

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,7 +1,7 @@
 Set-DNSClientServerAddress -InterfaceIndex 2 -ServerAddresses '10.118.86.66'
 
-$Password = 'NH88manR'
+$Password = 'BKzHGy5c'
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("administrator", $secpasswd)
 
-add-computer –domainname nsxguru.internal -Credential $mycreds -restart –force
+add-computer –domainname nsxguru.internal -Credential $mycreds -Restart -Force
