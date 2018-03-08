8db98e56de7f577d0d1163101ac705e3cb3f561e
amarzulli <andy.j@outlook.com>
2018-03-08 11:08:50 +1100

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,4 +1,5 @@
 Set-DNSClientServerAddress -InterfaceIndex 2 -ServerAddresses '10.118.86.66'
+
 $Password = 'NH88manR'
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("administrator", $secpasswd)
