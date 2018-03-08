807d400ac1b0105acf0088ca4c15803c9646c1fc
amarzulli <andy.j@outlook.com>
2018-03-08 15:39:28 +1100

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,6 +1,6 @@
 Set-DNSClientServerAddress -InterfaceAlias 'PrivateNetwork-A' -ServerAddresses '10.118.86.66'
 Install-WindowsFeature -Name 'Hyper-V' -IncludeAllSubFeature -IncludeManagementTools
-$Password = 'BKzHGy5c'
+$Password = 'Csa5hsuf'
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("administrator", $secpasswd)
 add-computer -DomainName 'nsxguru.internal' -Credential $mycreds -Restart -Force
