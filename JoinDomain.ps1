0e6c90947be201a1e025bca134ba3df12220a2a0
amarzulli <andy.j@outlook.com>
2018-03-08 16:44:25 +1100

    Update JoinDomain.ps1

--- JoinDomain.ps1
+++ JoinDomain.ps1
@@ -1,5 +1,5 @@
 Set-DNSClientServerAddress -InterfaceAlias 'PrivateNetwork-A' -ServerAddresses '10.118.86.66'
-Install-WindowsFeature -Name 'Hyper-V' -IncludeAllSubFeature -IncludeManagementTools
+Install-WindowsFeature -Name 'Hyper-V', 'Failover Clustering' -IncludeAllSubFeature -IncludeManagementTools
 $Password = 'Csa5hsuf'
 $secpasswd = ConvertTo-SecureString $Password -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("administrator", $secpasswd)
