088fe07b08cbea258d01d0b5bfa294ab4ba6cd72
amarzulli <andy.j@outlook.com>
2018-06-25 12:44:45 +1000

    Update getIpAddress.sh

--- getIpAddress.sh
+++ getIpAddress.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/getUserMetadata > ~/ip.address
+curl https://api.service.softlayer.com/rest/v3/SoftLayer_Resource_Metadata/getUserMetadata -o ~/ipaddress.txt
